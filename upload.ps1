72aede78837a0c71af5124ac07b7c9ece6baa44e
ronaldothegoat2026 <lokhh8682@gmail.com>
2025-01-13 11:30:10 +0700

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -1,19 +1,14 @@
-# Define the bot token and chat ID
 $botToken = "6595575260:AAEc_5Eojz0Gu7dfAWDo-LTcO7fQtiZ2Ido"
 $chatId = "-1002387709459"
 $filePath = "C:\path\to\your\file.txt"
 
-# Define the API URL
 $apiUrl = "https://api.telegram.org/bot$botToken/sendDocument"
 
-# Create a form data object
 $formData = @{
     chat_id = $chatId
     document = Get-Item -Path $filePath
 }
 
-# Send the file
 $response = Invoke-RestMethod -Uri $apiUrl -Method Post -Form $formData
 
-# Output the response
 $response
